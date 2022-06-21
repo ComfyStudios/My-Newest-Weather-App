f7a0e3886a654ba0ec6273e408ce16dab6625260
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-06-20 17:22:57 -0700

    changed JS script.js & html.html to index.html

--- script.js
+++ script.js
@@ -9,4 +9,5 @@ function displayTemperature(response) {
 let apiKey = "9ab263aeda80761b2d763070436d8afb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}d&units=Imperial`;
 
+let city = "west covina";
 axios.get(apiUrl).then(displayTemperature);
