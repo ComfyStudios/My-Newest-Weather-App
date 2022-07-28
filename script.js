c796479aaf79e0e80ce12579ec8f1573feca4e2d
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-07-27 19:04:51 -0700

    fixed cityElement

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  city.cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "2a953d8a53343c52593a07ae6489702d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}d&units=Imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}d&units=imperial`;
 
 let city = "west covina";
 axios.get(apiUrl).then(displayTemperature);
