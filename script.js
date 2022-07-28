8605a83c8c0a9f0431db121b31b94116cb367512
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-07-27 21:11:54 -0700

    added description element and span

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -14,7 +15,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "2a953d8a53343c52593a07ae6489702d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=west Covina&appid=2a953d8a53343c52593a07ae6489702d&units=Imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=west covina&appid=2a953d8a53343c52593a07ae6489702d&units=Imperial`;
 
-let city = "west covina";
+let city = "West Covina";
 axios.get(apiUrl).then(displayTemperature);
