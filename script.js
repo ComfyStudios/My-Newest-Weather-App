44e06a7cd98d4031f6ca69eafcd8afe8e4f749d4
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-08-02 19:52:28 -0700

    fixed weather descriptions

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let iconElement = document.querySelector("#icon");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  windElement.innerHTML = Math.round(response.data.wind.speed.description);
-  humidityElement.innerHTML = respose.data.main.humidity;
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "2a953d8a53343c52593a07ae6489702d";
