1734aa38f4db622ec2c6ac2e06cbc055157d99fc
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-08-09 18:47:32 -0700

    Image is not working

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
+  hoursElement.innerHTML = response.data.hours.main;
+  nowElement.innerHTML = `${hours}:${minutes}`;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
+let city = "West Covina";
 let apiKey = "2a953d8a53343c52593a07ae6489702d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=west Covina&appid=2a953d8a53343c52593a07ae6489702d&units=Imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=west covina&appid=2a953d8a53343c52593a07ae6489702d&units=Imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
 searchCity("West Covina");
