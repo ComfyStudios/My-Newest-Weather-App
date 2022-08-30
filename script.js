07701242a3fe210db879da946147497a6794d4ac
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-08-30 14:51:42 -0700

    added time and days

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -6,12 +24,14 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let iconElement = document.querySelector("#icon");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -34,11 +54,13 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
 
-  forecastHTML =
-    forecastHTML +
-    `
-  <div class="Weekly-forcast" id="forecast"> Monday</div>
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="Weekly-forcast" id="forecast">${day}</div>
   <div class="col-2">
   <img
   src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
@@ -50,26 +72,9 @@ function displayForecast() {
   <span class="weather-forecast-temperature-max"> 91° | </span>
   <span class="weather-forecast-temperature-min"> 75°</span>
   </div>
-  </div>`;
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-
-  forecastHTML =
-    forecastHTML +
-    `
-  <div class="Weekly-forcast" id="forecast"> Monday</div>
-  <div class="col-2">
-  <img
-  src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"
-  alt="weather"
-  width="42"
-  />
   </div>
-  <div class="Weather-forecast-temperature">
-  <span class="weather-forecast-temperature-max"> 91° | </span>
-  <span class="weather-forecast-temperature-min"> 75°</span>
-  </div>
-  </div>`;
+  `;
+  });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
