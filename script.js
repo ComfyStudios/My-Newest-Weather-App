4bd07ed087dea172d05bc34eb34a43663c75ca2f
Shelbyswork <106495176+Shelbyswork@users.noreply.github.com>
2022-09-03 19:16:02 -0700

    api and metric not working

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function formateDate(timestamp) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
-  axios.get(aprUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -48,8 +48,8 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=Imperial`;
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
